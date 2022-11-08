7f683e37a8002c8677e9f18c695546da9c127e24
ArtPolguy <art.polguy@gmail.com>
2022-11-08 18:17:52 +0200

    update_1task

--- js/task-01.js
+++ js/task-01.js
@@ -1,20 +1,8 @@
 const listItemRefs = document.querySelectorAll(`.item`);
+
 console.log(`Number of categories: ${listItemRefs.length}`);
 
-const firstListHeadRefs = listItemRefs[0].firstElementChild;
-console.log(`Category: ${firstListHeadRefs.textContent}`);
-
-const firstItemsRefs = listItemRefs[0].querySelectorAll(`li`);
-console.log(`Elements: ${firstItemsRefs.length}`);
-
-const secondListHeadRefs = listItemRefs[1].firstElementChild;
-console.log(`Category: ${secondListHeadRefs.textContent}`);
-
-const secondItemsRefs = listItemRefs[1].querySelectorAll(`li`);
-console.log(`Elements: ${secondItemsRefs.length}`);
-
-const thirdListHeadRefs = listItemRefs[2].firstElementChild;
-console.log(`Category: ${thirdListHeadRefs.textContent}`);
-
-const thirdItemsRefs = listItemRefs[2].querySelectorAll(`li`);
-console.log(`Elements: ${thirdItemsRefs.length}`);
+listItemRefs.forEach(function (el) {
+  console.log(`Category: ${el.firstElementChild.textContent}`);
+  console.log(`Elements: ${el.lastElementChild.children.length}`);
+});
